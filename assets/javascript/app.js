aed1eede77f5deda7f3374926b0865efcf1374de
Jacob Stanger <jstangwork@gmail.com>
2020-02-07 21:10:01 -0700

    wow my functions were getting ugly. cleaned them up nice using this keyword.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,58 +4,114 @@
 
 $(document).ready(function () {
 
-  $('#rock-one').on('click', function(){
-    $(this).css('color', '#333');
-    console.log(this.id);
-    
-    colorOthersGray(this.id);
+  function goGray(choice) {
+    console.log('goGray() ', choice);
+    var player = choice.substr(choice.length - 3);
+    $('#' + choice).css('color', '#333');
+
+    if (player === 'one') {
+      if (choice === 'rock-one') {
+        $('#paper-one').css('color', 'lightgrey');
+        $('#scissors-one').css('color', 'lightgrey');
+      } else if (choice === 'paper-one') {
+        $('#rock-one').css('color', 'lightgrey');
+        $('#scissors-one').css('color', 'lightgrey');
+      } else {
+        $('#paper-one').css('color', 'lightgrey');
+        $('#rock-one').css('color', 'lightgrey');
+      }
+
+
+    } else {
+      if (choice === 'rock-two') {
+        $('#paper-two').css('color', 'lightgrey');
+        $('#scissors-two').css('color', 'lightgrey');
+      } else if (choice === 'paper-two') {
+        $('#rock-two').css('color', 'lightgrey');
+        $('#scissors-two').css('color', 'lightgrey');
+      } else {
+        $('#paper-two').css('color', 'lightgrey');
+        $('#rock-two').css('color', 'lightgrey');
+      }
+    }
+
+  }
+
+  $('.one').on('click', function() {
+    // $(this).css('color', '#333');
+    goGray(this.id);
   });
 
-  $('#paper-one').on('click', function(){
-    $(this).css('color', '#333');
-    colorOthersGray(this.id);
-  });
-
-  $('#scissors-one').on('click', function(){
-    $(this).css('color', '#333');
-    colorOthersGray(this.id);
-  });
-
-  $('#rock-two').on('click', function(){
-    $(this).css('color', '#333');
-    colorOthersGray(this.id);
-  });
-
-  $('#paper-two').on('click', function(){
-    $(this).css('color', '#333');
-    colorOthersGray(this.id);
-  });
-
-  $('#scissors-two').on('click', function(){
-    $(this).css('color', '#333');
-    colorOthersGray(this.id);
+  $('.two').on('click', function() {
+    // $(this).css('color', '#333');
+    goGray(this.id);
   });
 
 
-  function colorOthersGray(choice) {
+  // $('#rock-one').on('click', function(){
+  //   $(this).css('color', '#333');
+  //   prepOthersForGray(this.id);
+  // });
+
+  // $('#paper-one').on('click', function(){
+  //   $(this).css('color', '#333');
+  //   prepOthersForGray(this.id);
+  // });
+
+  // $('#scissors-one').on('click', function(){
+  //   $(this).css('color', '#333');
+  //   prepOthersForGray(this.id);
+  // });
+
+  // $('#rock-two').on('click', function(){
+  //   $(this).css('color', '#333');
+  //   prepOthersForGray(this.id);
+  // });
+
+  // $('#paper-two').on('click', function(){
+  //   $(this).css('color', '#333');
+  //   prepOthersForGray(this.id);
+  // });
+
+  // $('#scissors-two').on('click', function(){
+  //   $(this).css('color', '#333');
+  //   prepOthersForGray(this.id);
+  // });
+
+
+  function prepOthersForGray(choice) {
     var whichPlayer = choice.substr(choice.length - 3);
-    console.log(whichPlayer);
-    // switch (choice) {
-    //   case 'rock-one':
-    //     $('#paper-one').css('color', 'lightgrey');
-    //     $('#scissors-one').css('color', 'lightgrey');
-    //     break;
-    //   case 'paper-one':
-    //     $('#rock-one').css('color', 'lightgrey');
-    //     $('#scissors-one').css('color', 'lightgrey');
-    //     break;
-    //   case 'scissors-one':
-    //     $('#rock-one').css('color', 'lightgrey');
-    //     $('#paper-one').css('color', 'lightgrey');
-    //     break;
-    //   default:
-    //     break;
-    // }
+    // console.log(whichPlayer);
+
+    if (whichPlayer === 'one') {
+      //======= Player 1 =============================
+      if (choice === 'rock-' + whichPlayer) {
+        $('#paper-' + whichPlayer).css('color', 'lightgrey');
+        $('#scissors-' + whichPlayer).css('color', 'lightgrey');
+      } else if (choice === 'paper-' + whichPlayer) {
+        $('#rock-' + whichPlayer).css('color', 'lightgrey');
+        $('#scissors-' + whichPlayer).css('color', 'lightgrey');
+      } else {
+        // scissors
+        $('#paper-' + whichPlayer).css('color', 'lightgrey');
+        $('#rock-' + whichPlayer).css('color', 'lightgrey');
+      }
+
+
+    } else {
+      //======= Player 2 =============================
+      if (choice === 'rock-' + whichPlayer) {
+        $('#paper-' + whichPlayer).css('color', 'lightgrey');
+        $('#scissors-' + whichPlayer).css('color', 'lightgrey');
+      } else if (choice === 'paper-' + whichPlayer) {
+        $('#rock-' + whichPlayer).css('color', 'lightgrey');
+        $('#scissors-' + whichPlayer).css('color', 'lightgrey');
+      } else {
+        // scissors
+        $('#paper-' + whichPlayer).css('color', 'lightgrey');
+        $('#rock-' + whichPlayer).css('color', 'lightgrey');
+      }
+    }
 
   }
 
