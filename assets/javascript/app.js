5ae11451579916a39770173ec1fa870697b59c71
Jacob Stanger <jstangwork@gmail.com>
2020-02-07 20:37:14 -0700

    good chuck of ui done. working on choice.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,3 +2,63 @@
 // Jacob Stanger
 // FEB 2020
 
+$(document).ready(function () {
+
+  $('#rock-one').on('click', function(){
+    $(this).css('color', '#333');
+    console.log(this.id);
+    
+    colorOthersGray(this.id);
+  });
+
+  $('#paper-one').on('click', function(){
+    $(this).css('color', '#333');
+    colorOthersGray(this.id);
+  });
+
+  $('#scissors-one').on('click', function(){
+    $(this).css('color', '#333');
+    colorOthersGray(this.id);
+  });
+
+  $('#rock-two').on('click', function(){
+    $(this).css('color', '#333');
+    colorOthersGray(this.id);
+  });
+
+  $('#paper-two').on('click', function(){
+    $(this).css('color', '#333');
+    colorOthersGray(this.id);
+  });
+
+  $('#scissors-two').on('click', function(){
+    $(this).css('color', '#333');
+    colorOthersGray(this.id);
+  });
+
+
+  function colorOthersGray(choice) {
+    var whichPlayer = choice.substr(choice.length - 3);
+    console.log(whichPlayer);
+    // switch (choice) {
+    //   case 'rock-one':
+    //     $('#paper-one').css('color', 'lightgrey');
+    //     $('#scissors-one').css('color', 'lightgrey');
+    //     break;
+    //   case 'paper-one':
+    //     $('#rock-one').css('color', 'lightgrey');
+    //     $('#scissors-one').css('color', 'lightgrey');
+    //     break;
+    //   case 'scissors-one':
+    //     $('#rock-one').css('color', 'lightgrey');
+    //     $('#paper-one').css('color', 'lightgrey');
+    //     break;
+    //   default:
+    //     break;
+    // }
+
+  }
+
+
+
+});
